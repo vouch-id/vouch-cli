89379340c75e306dabb86c2c41f4845eb8fc9356
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2022-08-11 15:25:47 +0200

    The Great Concept Renaming

--- script.py
+++ script.py
@@ -2,14 +2,14 @@ import argparse
 import jsonrpc
 
 class Script:
-    def __init__(self, method, keyFileNeeded=True):
+    def __init__(self, method, keyFile='keyFile'):
         self.method = method
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument('--insecure', action=argparse.BooleanOptionalAction)
         self.parser.add_argument('serviceUrl')
-        self.keyFileNeeded = keyFileNeeded
-        if keyFileNeeded:
-            self.parser.add_argument('keyFile')
+        self.keyFile = keyFile
+        if keyFile:
+            self.parser.add_argument(keyFile)
         self._args = None
 
     def args(self):
@@ -19,7 +19,7 @@ class Script:
 
     def run(self, params=None):
         args = self.args()
-        return jsonrpc.Client(args.keyFile if self.keyFileNeeded else None,
+        return jsonrpc.Client(getattr(args, self.keyFile) if self.keyFile else None,
                               args.serviceUrl + '_/rpc',
                               insecure=args.insecure).call(
                                   self.method,
